8b4b4820646f3f9157a3253a7df300544f7acdef
Jacky Fan <mrf1013@outlook.com>
2022-01-11 16:01:11 +0800

    create search page

---
+++ pages/search/[keywords].js
@@ -0,0 +1,39 @@
+import { useRouter } from 'next/router'
+import { Flex, Grid, GridItem, Spacer, Stack, Text } from '@chakra-ui/react'
+import MenuBar from "../../components/MenuBar";
+import FoldersPanel from "../../components/FoldersPanel";
+import AddItemButton from "../../components/AddItemButton";
+import TodoListTable from "../../components/TodoListTable";
+
+const Search = () => {
+    const router = useRouter();
+    const { keywords } = router.query;  //url variable
+
+
+    return (
+        <Grid templateColumns='repeat(5, 1fr)'>
+            <GridItem colSpan={5}>
+                <MenuBar />
+            </GridItem>
+            <GridItem>
+                <FoldersPanel />
+            </GridItem>
+            <GridItem colSpan={4}>
+                <Stack spacing={5} p={10}>
+                    <Flex>
+                        <Text>
+                            <Text as="b" fontSize="xl">
+                                Searching: {keywords}
+                            </Text>
+                        </Text>
+                        <Spacer />
+                        <AddItemButton />
+                    </Flex>
+                    <TodoListTable />
+                </Stack>
+            </GridItem>
+        </Grid>
+    );
+}
+
+export default Search;
