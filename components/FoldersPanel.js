415463b54296ca16aee5db9e444a691d06c10e5e
Jacky Fan <mrf1013@outlook.com>
2022-01-13 16:42:44 +0800

    implement temp route for folders panel

--- components/FoldersPanel.js
+++ components/FoldersPanel.js
@@ -18,6 +18,7 @@ import {
 } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 import { useToast } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 
 
 const ActiveButton = (props) => {
@@ -28,11 +29,12 @@ const ActiveButton = (props) => {
 
 const InactiveButton = (props) => {
     return (
-        <Button variant="outline"><Text color="gray.500">{props.children}</Text></Button>
+        <Button variant="outline" onClick={props.onClick}><Text color="gray.500">{props.children}</Text></Button>
     );
 }
 
 const AddFolderButton = (props) => {
+
 
     const { isOpen, onOpen, onClose } = useDisclosure()
 
@@ -81,16 +83,20 @@ const AddFolderButton = (props) => {
 }
 
 export default function FoldersPanel() {
+
+    const router = useRouter();
+
     return (
         <Stack w="100%" p={10}>
             <ActiveButton>Today</ActiveButton>
             <InactiveButton>Done</InactiveButton>
             <InactiveButton>All Todos</InactiveButton>
             <Divider w="100%" h={1} py={1} />
-            <InactiveButton>Folder 1</InactiveButton>
-            <InactiveButton>Folder 2</InactiveButton>
-            <InactiveButton>Folder 3</InactiveButton>
-            <InactiveButton>Folder 4</InactiveButton>
+            {/* temp route */}
+            <InactiveButton onClick={()=>{router.push("/folder/Folder1")}}>Folder 1</InactiveButton>
+            <InactiveButton onClick={()=>{router.push("/folder/Folder2")}}>Folder 2</InactiveButton>
+            <InactiveButton onClick={()=>{router.push("/folder/Folder3")}}>Folder 3</InactiveButton>
+            <InactiveButton onClick={()=>{router.push("/folder/Folder4")}}>Folder 4</InactiveButton>
             <AddFolderButton />
         </Stack>
     );
