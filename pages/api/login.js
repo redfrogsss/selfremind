ebb65ec4a12b32684ad387d0ecbd5696e424bee4
Jacky Fan <mrf1013@outlook.com>
2022-01-19 12:41:13 +0800

    force POST method in login api

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,32 +3,39 @@ var mysql = require('mysql');
 
 export default function handler(req, res) {
 
-    const username = req.query.username;
-    const pwd = req.query.pwd;
-
-    if(!username | !pwd){
-        res.status(200).json({ err: "Invalid username / password." });
-        return ;
-    }
-
-    var statements = "SELECT COUNT(*) AS result FROM auth WHERE username=? AND pwd=?;";
-    var insert = [username, pwd];
-    statements = mysql.format(statements, insert);
-
-    var con = mysql.createConnection(config);
-
-    con.connect(function (err) {
-        if (err) {
-            res.status(200).json({ err: err });
-        };
-        con.query(statements, function (err, result) {
-            if (err) { res.status(200).json({ err: err }); };
-            const authResult = (result[0].result === 1);
-            if(authResult === true) {
-                res.status(200).json({ authStatus: "success" });
-            } else {
-                res.status(200).json({ err: "Invalid username / password." });
-            }
+    if (req.method === 'POST') {
+        // Process a POST request
+        const username = req.query.username;
+        const pwd = req.query.pwd;
+    
+        if(!username | !pwd){
+            res.status(200).json({ err: "Invalid username / password." });
+            return ;
+        }
+    
+        var statements = "SELECT COUNT(*) AS result FROM auth WHERE username=? AND pwd=?;";
+        var insert = [username, pwd];
+        statements = mysql.format(statements, insert);
+    
+        var con = mysql.createConnection(config);
+    
+        con.connect(function (err) {
+            if (err) {
+                res.status(200).json({ err: err });
+            };
+            con.query(statements, function (err, result) {
+                if (err) { res.status(200).json({ err: err }); };
+                const authResult = (result[0].result === 1);
+                if(authResult === true) {
+                    res.status(200).json({ authStatus: "success" });
+                } else {
+                    res.status(200).json({ err: "Invalid username / password." });
+                }
+            });
         });
-    });
+      } else {
+        // Handle any other HTTP method
+        res.status(200).json({ err: "Invalid method." });
+      }
+
 }
