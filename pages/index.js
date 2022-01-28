4d70d3d27cb5546623cbcefedde778bcf5230672
Jacky Fan <mrf1013@outlook.com>
2022-01-28 10:12:22 +0800

    implement loggedin redirect

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+const axios = require('axios');
 
 export default function Home() {
-
+  const [cookies, setCookie, removeCookie] = useCookies(['userID']);
   const router = useRouter();
-  const href = "/login";
 
-  useEffect(()=>{
-    router.push(href);
-  },[]);
+  useEffect(() => {
+    if (!cookies.userID) {
+      router.push("/login");
+    } else {
+      axios.post("/api/authStatus", { userID: cookies.userID }).then((res) => {
+        if (res.data.authStatus) {
+          router.push("/home");
+        } else {
+          router.push("/login");
+        }
+      });
+    }
+  }, []);
 
   return (
     <p>Redirecting...</p>
