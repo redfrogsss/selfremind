1906ad2be4f7b670c5184f136160ea926c3c567c
Jacky Fan <mrf1013@outlook.com>
2022-01-28 15:49:01 +0800

    implement get basic data from frontend

--- components/AddItemButton.js
+++ components/AddItemButton.js
@@ -34,6 +34,25 @@ export default function AddItemButton() {
             isClosable: true,
         });
 
+    const submitHandler = (e) => {
+        e.preventDefault();
+        
+        var data = {
+            name: e.target.name.value,
+            description: e.target.description.value,
+            datetime: undefined,
+            reminder: e.target.reminder.value,
+            repeat: e.target.repeat.value,
+            folder: e.target.folder.value
+        }
+        
+        console.log(data);
+
+        onClose();
+        SuccessToast();
+    }
+
+
     return (
         <>
             <Button
@@ -45,64 +64,66 @@ export default function AddItemButton() {
             </Button>
 
             <Modal isOpen={isOpen} onClose={onClose} size='xl'>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Add Item</ModalHeader>
-                    <ModalCloseButton />
+                <form onSubmit={submitHandler}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Add Item</ModalHeader>
+                        <ModalCloseButton />
 
-                    <ModalBody>
-                        <Stack spacing={3}>
-                            <FormControl>
-                                <FormLabel htmlFor='name'>Name</FormLabel>
-                                <Input id='name' type='text' placeholder="Shopping" />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor='description'>Description</FormLabel>
-                                <Textarea id='desciption ' placeholder='Description' />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor='description'>Date & Time</FormLabel>
-                                <DateTimePicker />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor='reminder'>Reminder</FormLabel>
-                                <Select id='reminder'>
-                                    <option value='none' defaultChecked>No Reminder</option>
-                                    <option value='5'>5 minutes before</option>
-                                    <option value='10'>10 minutes before</option>
-                                    <option value='15'>15 minutes before</option>
-                                    <option value='30'>30 minutes before</option>
-                                    <option value='60'>1 hour before</option>
-                                </Select>
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor='reminder'>Repeat</FormLabel>
-                                <Select id='repeat'>
-                                    <option value='none' defaultChecked>None</option>
-                                    <option value='daily'>Daily</option>
-                                    <option value='weekly'>Weekly</option>
-                                    <option value='monthly'>Monthly</option>
-                                </Select>
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor='folder'>Folder</FormLabel>
-                                <Select id='folder'>
-                                    <option value='folder1' defaultChecked>Folder 1</option>
-                                    <option value='folder2'>Folder 2</option>
-                                    <option value='folder3'>Folder 3</option>
-                                    <option value='folder4'>Folder 4</option>
-                                </Select>
-                            </FormControl>
-                        </Stack>
-                    </ModalBody>
+                        <ModalBody>
+                            <Stack spacing={3}>
+                                <FormControl isRequired>
+                                    <FormLabel htmlFor='name'>Name</FormLabel>
+                                    <Input id='name' type='text' placeholder="Shopping" />
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel htmlFor='description'>Description</FormLabel>
+                                    <Textarea id='description' placeholder='Description' />
+                                </FormControl>
+                                <FormControl isRequired>
+                                    <FormLabel htmlFor='datetime'>Date & Time</FormLabel>
+                                    <DateTimePicker />
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel htmlFor='reminder'>Reminder</FormLabel>
+                                    <Select id='reminder'>
+                                        <option value='none' defaultChecked>No Reminder</option>
+                                        <option value='5'>5 minutes before</option>
+                                        <option value='10'>10 minutes before</option>
+                                        <option value='15'>15 minutes before</option>
+                                        <option value='30'>30 minutes before</option>
+                                        <option value='60'>1 hour before</option>
+                                    </Select>
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel htmlFor='repeat'>Repeat</FormLabel>
+                                    <Select id='repeat'>
+                                        <option value='none' defaultChecked>None</option>
+                                        <option value='daily'>Daily</option>
+                                        <option value='weekly'>Weekly</option>
+                                        <option value='monthly'>Monthly</option>
+                                    </Select>
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel htmlFor='folder'>Folder</FormLabel>
+                                    <Select id='folder'>
+                                        <option value='folder1' defaultChecked>Folder 1</option>
+                                        <option value='folder2'>Folder 2</option>
+                                        <option value='folder3'>Folder 3</option>
+                                        <option value='folder4'>Folder 4</option>
+                                    </Select>
+                                </FormControl>
+                            </Stack>
+                        </ModalBody>
 
-                    <ModalFooter>
-                        <Button colorScheme='teal' mr={3} onClick={() => { onClose(); SuccessToast(); }} >Add Item</Button>
-                        <Button colorScheme='teal' variant='ghost' onClick={onClose}>
-                            Cancel
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
+                        <ModalFooter>
+                            <Button colorScheme='teal' mr={3} type="submit">Add Item</Button>
+                            <Button colorScheme='teal' variant='ghost' onClick={onClose}>
+                                Cancel
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </form>
             </Modal>
         </>
     );
