ff0c7de6dca4b83e5ca843710ec8aa830675c7b4
Jacky Fan <mrf1013@outlook.com>
2022-01-28 16:08:47 +0800

    modify init SQL code

--- config/init.sql
+++ config/init.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS auth;
+
 DROP TABLE IF EXISTS items;
+
+DROP TABLE IF EXISTS folders;
 
 CREATE TABLE auth (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -12,16 +15,32 @@ INSERT INTO
 VALUES
     ('admin', 'admin');
 
+CREATE TABLE folders (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userID INT NOT NULL,
+    name VARCHAR(255)
+);
+
+INSERT INTO
+    folders (userID, name)
+VALUES
+    (1, 'To-do');
+
 CREATE TABLE items (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     userID INT NOT NULL,
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     description VARCHAR(500),
-    datetime TIMESTAMP,
+    datetime TIMESTAMP NOT NULL,
     reminder INT,
     repeats VARCHAR(16),
-    folder VARCHAR(255),
-    finished BOOLEAN
+    folder INT NOT NULL,
+    finished BOOLEAN NOT NULL DEFAULT 0 
 );
+
+INSERT INTO
+    items (userID, name, description, datetime, reminder, repeats, folder)
+VALUES
+    (1, 'First Item', 'Remind me to do shopping', now(), 0, 'none', 1);
 
 SHOW TABLES;
