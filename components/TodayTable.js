e8f843957559be3899c8cfef6c9219daa34ebff8
Jacky Fan <mrf1013@outlook.com>
2022-01-29 19:41:35 +0800

    implement filter data

--- components/TodayTable.js
+++ components/TodayTable.js
@@ -2,16 +2,23 @@ import { useEffect, useState } from "react";
 import TodoListTable from "./TodoListTable";
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
+import moment from "moment";
 
 export default function TodayTable () {
     const [cookies, setCookie, removeCookie] = useCookies(['userID']);
 
     const [data, setData] = useState(undefined);
+
+    const filteredData = (data) => {
+        return data.filter(((value)=>{  // return true or false
+            return moment().isSame(moment(value.datetime), "date");
+        }));
+    }
     
     useEffect(()=>{
         const url = "/api/items?userID=" + cookies.userID;
         axios.get(url).then((res)=>{
-            setData(res.data.result);
+            setData(filteredData(res.data.result));
         }).catch((err)=>{
             console.error(err);
         });
