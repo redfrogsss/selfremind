8625797da83f46426c82ee773a8b2c1dccb466c4
Jacky Fan <mrf1013@outlook.com>
2022-01-29 20:22:02 +0800

    fix index.js redirect

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import AuthRedirect from '../components/AuthRedirect';
+import { useRouter } from 'next/router'
+import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+const axios = require('axios');
 
 export default function Home() {
+
+  const [cookies, setCookie, removeCookie] = useCookies(['userID']);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!cookies.userID) {
+      router.push("/login");
+    } else {
+      axios.post("/api/authStatus", { userID: cookies.userID }).then((res) => {
+        if (!res.data.authStatus) {
+          router.push("/login");
+        } else {
+          router.push("/home");
+        }
+      });
+    }
+  }, []);
+
   return (
     <>
-      <AuthRedirect />
       <p>Redirecting...</p>
     </>
   )
