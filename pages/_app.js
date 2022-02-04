7f4e1e1cfc934816986dcd6c09e80d0c21ab2cfc
Jacky Fan <mrf1013@outlook.com>
2022-02-04 11:43:50 +0800

    add title to page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { CookiesProvider } from "react-cookie"
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <CookiesProvider>
+        <Head><title>SelfRemind</title></Head>
         <Component {...pageProps} />
       </CookiesProvider>
     </ChakraProvider>
